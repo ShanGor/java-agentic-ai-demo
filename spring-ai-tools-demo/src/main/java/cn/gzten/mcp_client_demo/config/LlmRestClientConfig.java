c496fb0d9fa61f44c3c265aeec199374da2431c9
Samuel Chan <emschan@live.cn>
2025-03-23 17:47:38 +0800

    Test image

--- spring-ai-tools-demo/src/main/java/cn/gzten/mcp_client_demo/config/LlmRestClientConfig.java
+++ spring-ai-tools-demo/src/main/java/cn/gzten/mcp_client_demo/config/LlmRestClientConfig.java
@@ -5,8 +5,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.ReactorClientHttpRequestFactory;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.client.RestClient;
 
+import org.springframework.web.reactive.function.client.WebClient;
 import reactor.netty.http.client.HttpClient;
 
 /**
@@ -16,7 +18,7 @@ import reactor.netty.http.client.HttpClient;
 @Slf4j
 public class LlmRestClientConfig {
     @Bean
-    RestClient llmRestClient(CustomAiMcp ai) {
+    RestClient.Builder llmRestClient(CustomAiMcp ai) {
         var responseTimeout = ai.getAi().getResponseTimeout();
         log.info("llmRestClient responseTimeout: {} seconds", responseTimeout.getSeconds());
         HttpClient client = HttpClient.create()
@@ -24,7 +26,16 @@ public class LlmRestClientConfig {
         ClientHttpRequestFactory factory = new ReactorClientHttpRequestFactory(client);
 
         return RestClient.builder()
-                .requestFactory(factory)
-                .build();
+                .requestFactory(factory);
+    }
+
+    @Bean
+    WebClient.Builder llmWebClient(CustomAiMcp ai) {
+        var responseTimeout = ai.getAi().getResponseTimeout();
+        log.info("llmWebClient responseTimeout: {} seconds", responseTimeout.getSeconds());
+        HttpClient client = HttpClient.create()
+                .responseTimeout(responseTimeout);
+
+        return WebClient.builder().clientConnector(new ReactorClientHttpConnector(client));
     }
 }
